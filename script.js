922277714739609fedb3e9563bacd67e8c3acc7f
Conner <connerclere@gmail.com>
2021-12-20 15:34:22 -0600

    Add basic functions for operators

---
+++ script.js
@@ -0,0 +1,25 @@
+function add(num1, num2) {
+    return num1 + num2;
+}
+
+function subtract(num1, num2) {
+    return num1 - num2;
+}
+
+function multiply(num1, num2) {
+    return num1 * num2;
+}
+
+function divide(num1, num2) {
+    return num1 / num2;
+}
+
+function operate(operator, num1, num2) {
+    switch(operator){
+        case "+": return add(num1, num2); break;
+        case "-": return subtract(num1, num2); break;
+        case "*": return multiply(num1, num2); break;
+        case "/": return divide(num1, num2); break;
+    }
+
+}
